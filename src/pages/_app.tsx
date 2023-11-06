4ef9679db3751ca12c9b0060c245c3757f43c64b
Jacob YC <jacob.yc.wang@foxconn.com>
2023-11-06 16:27:47 +0800

    Add different colored icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import AuthGaurd from "~/components/AuthGuard";
 import Layout from "~/components/Layout";
 
 import "leaflet/dist/leaflet.css";
+import { useRouter } from "next/router";
 import SentrySetup from "~/components/SentrySetup";
 import { SiteProvider } from "~/context/SiteContext";
 import "~/styles/globals.css";
@@ -19,6 +20,18 @@ const MyApp: AppType<{ session: Session | null; ogMeta?: OGMetaProps }> = ({
   Component,
   pageProps: { session, ogMeta, ...pageProps },
 }) => {
+  const router = useRouter();
+  const isYideclass = router.pathname.split("/")[1] === "yideclass";
+  const isVolunteer = router.pathname.split("/")[1] === "volunteer";
+
+  let siteIcon = "/logo512.png";
+  if (isYideclass) siteIcon = "/yideclass_logo.png";
+  else if (isVolunteer) siteIcon = "/volunteer_logo.png";
+
+  let themeColor = "";
+  if (isYideclass) themeColor = "#d1c0d8";
+  else if (isVolunteer) themeColor = "#d69c6c";
+
   return (
     <>
       <Head>
@@ -32,11 +45,8 @@ const MyApp: AppType<{ session: Session | null; ogMeta?: OGMetaProps }> = ({
           property="og:description"
           content={ogMeta?.ogDescription ?? "天一聖道院資訊系統"}
         ></meta>
-        <meta
-          property="og:image"
-          content={ogMeta?.ogImage ?? "/logo512.png"}
-        ></meta>
-        <meta name="theme-color" content="#836b5d" />
+        <meta property="og:image" content={ogMeta?.ogImage ?? siteIcon}></meta>
+        <meta name="theme-color" content={themeColor} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Analytics />
