b311bc6ad958c05354510ac4d9f9b710c1214239
Jacob YC <jacob.yc.wang@foxconn.com>
2023-11-24 00:09:50 +0800

    Add user filter

--- src/pages/volunteer/admin/casual/index.tsx
+++ src/pages/volunteer/admin/casual/index.tsx
@@ -1,5 +1,7 @@
-import { isEmpty, sortBy } from "lodash";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { includes, isEmpty, sortBy } from "lodash";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 import { AlertWarning } from "~/components/Alert";
 import { api } from "~/utils/api";
 
@@ -11,6 +13,10 @@ export default function AdminCasualUserList() {
     error: usersError,
   } = api.user.getUsers.useQuery({});
 
+  const { register, watch } = useForm<{ username: string }>({
+    mode: "all",
+  });
+
   if (usersIsLoading) return <div className="loading" />;
   if (!isEmpty(usersError))
     return <AlertWarning>{usersError.message}</AlertWarning>;
@@ -20,6 +26,17 @@ export default function AdminCasualUserList() {
       <article className="prose">
         <h1>日常工作管理</h1>
       </article>
+      <div className="divider">
+        <MagnifyingGlassIcon className="h-8 w-8" />
+        篩選
+      </div>
+      <input
+        type="text"
+        placeholder="姓名"
+        className="tiani-input"
+        {...register("username")}
+      />
+      <div className="divider" />
       <div className="overflow-x-auto">
         <table className="table">
           <thead>
@@ -28,17 +45,26 @@ export default function AdminCasualUserList() {
             </tr>
           </thead>
           <tbody>
-            {sortBy(users, "id").map((user) => (
-              <tr
-                key={user.id}
-                className="hover hover:cursor-pointer"
-                onClick={() =>
-                  void router.push(`/volunteer/admin/casual/${user.id}`)
-                }
-              >
-                <td>{user.name}</td>
-              </tr>
-            ))}
+            {sortBy(users, "id")
+              .filter(
+                (user) =>
+                  isEmpty(watch("username")) ||
+                  includes(
+                    user.name?.toLowerCase(),
+                    watch("username")?.toLowerCase(),
+                  ),
+              )
+              .map((user) => (
+                <tr
+                  key={user.id}
+                  className="hover hover:cursor-pointer"
+                  onClick={() =>
+                    void router.push(`/volunteer/admin/casual/${user.id}`)
+                  }
+                >
+                  <td>{user.name}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
