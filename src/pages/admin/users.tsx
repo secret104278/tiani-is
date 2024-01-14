49110a69e8ca70cbdcf2224124fca0936dec0a0f
Jacob Wang <secret104278@gmail.com>
2024-01-14 09:18:02 +0800

    Add sortable in user admin

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { BarsArrowDownIcon, PlusIcon } from "@heroicons/react/20/solid";
 import { Role } from "@prisma/client";
 import { isEmpty } from "lodash";
 import { useSession } from "next-auth/react";
@@ -63,6 +63,40 @@ function CreateUserDialogContent() {
   );
 }
 
+enum SortedType {
+  NAME,
+  IS_TIANI_ADMIN,
+  IS_VOLUNTEER_ADMIN,
+  IS_YIDECLASS_ADMIN,
+}
+
+const getComparator = (sortedType: SortedType) => {
+  type User = { name: string | null; roles: Role[] };
+
+  const rolesComparator = (role: Role) => (a: User, b: User) =>
+    Number(b.roles.includes(role)) - Number(a.roles.includes(role));
+
+  switch (sortedType) {
+    case SortedType.NAME:
+      return userComparator;
+    case SortedType.IS_TIANI_ADMIN:
+      return (a: User, b: User) => {
+        const result = rolesComparator(Role.TIANI_ADMIN)(a, b);
+        return result !== 0 ? result : userComparator(a, b);
+      };
+    case SortedType.IS_VOLUNTEER_ADMIN:
+      return (a: User, b: User) => {
+        const result = rolesComparator(Role.VOLUNTEER_ADMIN)(a, b);
+        return result !== 0 ? result : userComparator(a, b);
+      };
+    case SortedType.IS_YIDECLASS_ADMIN:
+      return (a: User, b: User) => {
+        const result = rolesComparator(Role.YIDECLASS_ADMIN)(a, b);
+        return result !== 0 ? result : userComparator(a, b);
+      };
+  }
+};
+
 export default function AdminUsersPage() {
   const {
     data: users,
@@ -84,6 +118,8 @@ export default function AdminUsersPage() {
     });
 
   const [createUserDialogOpen, setCreateUserDialogOpen] = useState(false);
+
+  const [sortedType, setSortedType] = useState(SortedType.NAME);
 
   const { data: sessionData } = useSession();
 
@@ -116,26 +152,60 @@ export default function AdminUsersPage() {
         <table className="table">
           <thead>
             <tr>
-              <th className="tiani-table-pin-col">姓名</th>
-              <th>
-                最高
-                <br />
-                管理者
+              <th className="tiani-table-pin-col">
+                <div
+                  className="flex cursor-pointer"
+                  onClick={() => setSortedType(SortedType.NAME)}
+                >
+                  姓名
+                  {sortedType === SortedType.NAME && (
+                    <BarsArrowDownIcon className="ml-1 w-4" />
+                  )}
+                </div>
               </th>
               <th>
-                天一志工隊
-                <br />
-                管理者
+                <div
+                  className="flex cursor-pointer"
+                  onClick={() => setSortedType(SortedType.IS_TIANI_ADMIN)}
+                >
+                  最高
+                  <br />
+                  管理者
+                  {sortedType === SortedType.IS_TIANI_ADMIN && (
+                    <BarsArrowDownIcon className="ml-1 w-4" />
+                  )}
+                </div>
               </th>
               <th>
-                義德班務網
-                <br />
-                管理者
+                <div
+                  className="flex cursor-pointer"
+                  onClick={() => setSortedType(SortedType.IS_VOLUNTEER_ADMIN)}
+                >
+                  天一志工隊
+                  <br />
+                  管理者
+                  {sortedType === SortedType.IS_VOLUNTEER_ADMIN && (
+                    <BarsArrowDownIcon className="ml-1 w-4" />
+                  )}
+                </div>
+              </th>
+              <th>
+                <div
+                  className="flex cursor-pointer"
+                  onClick={() => setSortedType(SortedType.IS_YIDECLASS_ADMIN)}
+                >
+                  義德班務網
+                  <br />
+                  管理者
+                  {sortedType === SortedType.IS_YIDECLASS_ADMIN && (
+                    <BarsArrowDownIcon className="ml-1 w-4" />
+                  )}
+                </div>
               </th>
             </tr>
           </thead>
           <tbody>
-            {users?.sort(userComparator)?.map((user) => (
+            {users?.sort(getComparator(sortedType))?.map((user) => (
               <tr key={user.id} className="hover">
                 <td className="tiani-table-pin-col">{user.name}</td>
                 <td>
