5e843edff3928cf477c536c12533b95f0f444f6f
secret104278 <secret104278@gmail.com>
2025-01-04 21:54:54 +0800

    wip

--- src/app/_components/line-image.tsx
+++ src/app/_components/line-image.tsx
@@ -2,5 +2,7 @@ import type { ImageProps } from "next/image";
 import Image from "next/image";
 
 export default function LineImage(props: ImageProps) {
-  return <Image width={800} height={800} {...props} />;
+  return (
+    <Image width={800} height={800} {...props} alt={props.alt ?? "line"} />
+  );
 }
