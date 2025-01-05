69ff7a6b7a0c17a894d7f44a7b51c7721346c857
secret104278 <secret104278@gmail.com>
2025-01-05 13:54:50 +0800

    fix: correct the locale

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,8 @@ const innerConfig = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["zh-TW"],
+    defaultLocale: "zh-TW",
   },
 
   images: {
